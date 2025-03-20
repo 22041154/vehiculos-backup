da23f4f55db775f2d962a90a65f66218caf3c984
22041154 <22041154@itdurango.edu.mx>
2025-03-19 22:40:11 -0600

    Configuración de CI/CD para respaldos automáticos

---
+++ backup.sh
@@ -0,0 +1,19 @@
+TIMESTAMP=$(date +"%F-%H-%M-%S")
+BACKUP_DIR="./backups"
+CONTAINER_NAME="mysqlDB2"
+DB_NAME="mysqlD2"
+DB_USER="root"
+DB_PASSWORD="1234"
+
+
+mkdir -p "$BACKUP_DIR"
+
+docker exec "$CONTAINER_NAME" /usr/bin/mysqldump -u"$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$BACKUP_DIR/backup_$TIMESTAMP.sql"
+
+
+if [ $? -eq 0 ]; then
+  echo "Backup completado: $BACKUP_DIR/backup_$TIMESTAMP.sql"
+else
+  echo "Error al crear el backup"
+  exit 1
+fi
